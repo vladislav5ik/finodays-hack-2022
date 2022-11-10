15821691c5302e766050e4a27b045dbf682baa33
vladislav5ik <spigin.vlad@mail.ru>
2022-11-10 11:03:17 +0300

    added commands MIN and MAX

--- scoring.py
+++ scoring.py
@@ -1,8 +1,11 @@
 from db import Filter
+import re
+
 def score(filters, filter_values):
     result = True
     for filter in filters:
         value = filter_values[filter.id]
+        value = apply_formula(filter, filters, filter_values)
         value_const = filter.value_const
         operation = filter.operation
         if operation == '>':
@@ -21,3 +24,29 @@ def score(filters, filter_values):
         return (False, f'Фильтр "{filter.name}" не пройден, условие неверно: {value} {operation} {value_const}.')
     else:
         return (True, 'Все фильтры пройдены.')
+
+def apply_formula(target_filter, all_filters, filter_values):
+    # find MIN MAX formula containing names of filters (e.g. MIN(roi, term))
+    m = re.search(r'(?P<operation>MIN|MAX)\((?P<filters>(\w+,?)+)\)', target_filter.name)
+    if m is None:
+        return filter_values[target_filter.id]
+    
+    operation = m.group('operation')
+    filters_in_formula = m.group('filters').split(',')
+    
+    # print(', '.join(filters_in_formula))
+
+    # find values of filters
+    for filter in all_filters:
+        if filter.name in filters_in_formula:
+            filters_in_formula[filters_in_formula.index(filter.name)] = filter_values[filter.id]
+    
+    # print(', '.join(filters_in_formula))
+    
+    # apply formula
+    if operation == 'MIN':
+        result = min(filters_in_formula)
+    elif operation == 'MAX':
+        result = max(filters_in_formula)
+
+    return result
